4468b9536fafbe630142dac927458b35884956b0
vsatamraju <vsatamraju@gmail.com>
2019-12-13 19:55:22 +0530

    Initial commit for Dictionary Example

---
+++ DictionarySample/DictionarySample/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DictionarySample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a string");
+            string str = Console.ReadLine();
+
+            //Using LINQ
+
+            var counted = str.GroupBy(g => g).Select(s => new { s.Key, Count = s.Count() }).OrderBy(o => o.Key);
+                 
+
+            foreach (var val in counted)
+            {
+                if (val.Count > 1)
+                {
+                    Console.Write(val.Key + "" + val.Count);
+                }
+                else
+                {
+                    Console.Write(val.Key);
+                }
+            }
+
+            //Using DICTIONARY
+
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (dict.ContainsKey(str[i]))
+                    dict[str[i]]++;
+                else
+                    dict.Add(str[i], 1);
+            }
+
+            foreach(KeyValuePair<char,int> pair in dict.OrderBy(e => e.Key))
+            {
+                if (pair.Value > 1)
+                {
+                    Console.Write(pair.Key + "" + pair.Value);
+                }
+                else
+                    Console.Write(pair.Key);
+            }
+
+            Console.ReadLine();
+           
+        }
+    }
+}
