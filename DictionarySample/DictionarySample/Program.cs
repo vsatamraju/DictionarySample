d0f2eed0bbb1ea8712feb434e94a181738e25929
Sai Kishore Chowdam (Zen3 Infosolutions America Inc) <v-sachow@microsoft.com>
2020-01-06 22:58:39 +0530

    Commented to complete dictionary

--- DictionarySample/DictionarySample/Program.cs
+++ DictionarySample/DictionarySample/Program.cs
@@ -31,28 +31,28 @@ namespace DictionarySample
             }
             Console.WriteLine("");
             Console.WriteLine("##############");
-            //Using DICTIONARY
-            Console.WriteLine("Using Dictionary");
-            Console.WriteLine("-----------------");
-            Dictionary<char, int> dict = new Dictionary<char, int>();
+            ////Using DICTIONARY
+            //Console.WriteLine("Using Dictionary");
+            //Console.WriteLine("-----------------");
+            //Dictionary<char, int> dict = new Dictionary<char, int>();
 
-            for (int i = 0; i < str.Length; i++)
-            {
-                if (dict.ContainsKey(str[i]))
-                    dict[str[i]]++;
-                else
-                    dict.Add(str[i], 1);
-            }
+            //for (int i = 0; i < str.Length; i++)
+            //{
+            //    if (dict.ContainsKey(str[i]))
+            //        dict[str[i]]++;
+            //    else
+            //        dict.Add(str[i], 1);
+            //}
 
-            foreach(KeyValuePair<char,int> pair in dict.OrderBy(e => e.Key))
-            {
-                if (pair.Value > 1)
-                {
-                    Console.Write(pair.Key + "" + pair.Value);
-                }
-                else
-                    Console.Write(pair.Key);
-            }
+            //foreach(KeyValuePair<char,int> pair in dict.OrderBy(e => e.Key))
+            //{
+            //    if (pair.Value > 1)
+            //    {
+            //        Console.Write(pair.Key + "" + pair.Value);
+            //    }
+            //    else
+            //        Console.Write(pair.Key);
+            //}
 
             Console.ReadLine();
            
