d008b536887e8d5843955a96fb9e61fed573f7ab
Sai Kishore Chowdam (Zen3 Infosolutions America Inc) <v-sachow@microsoft.com>
2020-01-06 23:53:19 +0530

    Separated the Files

---
+++ DictionarySample/DictionarySample/Linq.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionarySample
+{
+    class Linq
+    {
+        public void GetCountUsingLinq(string str)
+        {  
+            Console.WriteLine("Linq Code is Simple Than Dictionary");
+            Console.WriteLine("Using LINQ");
+            Console.WriteLine("---------------");
+            var counted = str.GroupBy(g => g).Select(s => new { s.Key, Count = s.Count() }).OrderBy(o => o.Key);
+            
+            foreach (var val in counted)
+            {
+                if (val.Count > 1)
+                {
+                    Console.Write(val.Key + "" + val.Count);
+                }
+                else
+                {
+                    Console.Write(val.Key);
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine("##############");
+        }       
+           
+    }
+}
---
+++ DictionarySample/DictionarySample/dictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionarySample
+{
+    class Dictionary
+    {
+        public void GetCountUsingDict(string str)
+        {
+            Console.WriteLine("Using Dictionary");
+            Console.WriteLine("-----------------");
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (dict.ContainsKey(str[i]))
+                    dict[str[i]]++;
+                else
+                    dict.Add(str[i], 1);
+            }
+
+            foreach (KeyValuePair<char, int> pair in dict.OrderBy(e => e.Key))
+            {
+                if (pair.Value > 1)
+                {
+                    Console.Write(pair.Key + "" + pair.Value);
+                }
+                else
+                    Console.Write(pair.Key);
+            }
+
+        }
+    }
+}
